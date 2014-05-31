056dc43eb29a6b40c936f1c356453eb509454f6e
pkozlowski <piotrkozlowski91@gmail.com>
2014-05-31 18:42:39 +0200

    Created NumericEventFilter

--- src/main/java/org/pwr/ii/gui/components/NumericEventFilter.java
+++ src/main/java/org/pwr/ii/gui/components/NumericEventFilter.java
@@ -1,7 +1,6 @@
 package org.pwr.ii.gui.components;
 
 import javafx.event.EventHandler;
-import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 
 /**
@@ -9,16 +8,14 @@ import javafx.scene.input.KeyEvent;
  * Date: 2014-05-31.
  */
 
-    public class NumericEventFilter implements EventHandler<KeyEvent> {
+public class NumericEventFilter implements EventHandler<KeyEvent> {
 
-        @Override
-        public void handle( KeyEvent t ) {
-            char ar[] = t.getCharacter().toCharArray();
-            char ch = ar[t.getCharacter().toCharArray().length - 1];
-            if (!(ch >= '0' && ch <= '9')) {
-                System.out.println("The char you entered is not a number");
-                t.consume();
-            }
-        }
-
+    @Override
+    public void handle(KeyEvent t) {
+        char ar[] = t.getCharacter().toCharArray();
+        char ch = ar[t.getCharacter().toCharArray().length - 1];
+        if (!(ch >= '0' && ch <= '9'))
+            t.consume();
     }
+
+}
