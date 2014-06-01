2a86d0f5887bb4fb8a95e7268e3f8bd9d0214bd0
pkozlowski <piotrkozlowski91@gmail.com>
2014-06-01 13:54:26 +0200

    Added SizeCriterion

---
+++ src/main/java/org/pwr/ii/criteria/SizeCriterion.java
@@ -0,0 +1,21 @@
+package org.pwr.ii.criteria;
+
+/**
+ * Author: Piotr Kozłowski
+ * Date: 2014-06-01.
+ */
+public class SizeCriterion extends Criterion {
+    private final double size;
+    private final double maxSize;
+
+    public SizeCriterion(double factor, double size, double maxSize) {
+        super(factor);
+        this.size = size;
+        this.maxSize = maxSize;
+    }
+
+    @Override
+    public double calculate(Alcohol alcohol) {
+        return 1 - Math.abs(alcohol.getSize()-size)/maxSize;
+    }
+}
---
+++ src/test/java/org/pwr/ii/criteria/SizeCriterionTest.java
@@ -0,0 +1,26 @@
+package org.pwr.ii.criteria;
+
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class SizeCriterionTest {
+
+    @Test
+    public void should_return_1_when_equals_to_criterion() throws Exception {
+        Alcohol alcohol = mock(Alcohol.class);
+        when(alcohol.getSize()).thenReturn(700d);
+        Criterion sizeCriterion = new SizeCriterion(1,700,700);
+
+        assertThat(sizeCriterion.calculate(alcohol)).isEqualTo(1);
+    }
+    @Test
+    public void should_return_0_when_far_from_criterion() throws Exception {
+        Alcohol alcohol = mock(Alcohol.class);
+        when(alcohol.getSize()).thenReturn(0d);
+        Criterion sizeCriterion = new SizeCriterion(1,700,700);
+        assertThat(sizeCriterion.calculate(alcohol)).isEqualTo(0);
+    }
+}
